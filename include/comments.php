2af45e70dcfc7b302f7449503ecf8f04888fd6ae
davbre-3 <davbre-3@student.ltu.se>
2026-02-26 14:11:54 +0100

    username in comments

--- include/comments.php
+++ include/comments.php
@@ -26,7 +26,10 @@
 
         $prod_id =$_GET["id"];
 
-        $query = $db->prepare("SELECT * FROM comments WHERE product_id=:prod_id");
+        $query = $db->prepare("SELECT * FROM comments c 
+        inner join users 
+        on c.user_id =users.user_id 
+        WHERE product_id=:prod_id");
         $query->bindParam(':prod_id', $prod_id);
         $query->execute();
         while($row = $query->fetch()){
