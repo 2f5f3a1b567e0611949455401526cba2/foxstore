a56fadcbdd5123e12275220fa5b3096bc5fd9024
NitramiuZ <67265963+NitramiuZ@users.noreply.github.com>
2026-02-26 10:53:08 +0100

    Added image caption/alt editing (#38)

--- include/db.php
+++ include/db.php
@@ -4,9 +4,8 @@
 	$dbuser = "root";
 	$dbpass = "";
 	$dbinfo = "mysql:host=$host;dbname=store;charset=UTF8";
-
-	if (file_exists("../../config.php")) {
-		include "../../config.php";
+	if (file_exists(__DIR__."/../../config.php")) {
+		include __DIR__."/../../config.php";
 	}
 	$db = new PDO($dbinfo,$dbuser,$dbpass);
 ?>
