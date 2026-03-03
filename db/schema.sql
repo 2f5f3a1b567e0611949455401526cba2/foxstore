8bacc36585d291431faa991e2e87a1e20259cce8
RorrowingMadness <140700643+RorrowingMadness@users.noreply.github.com>
2026-03-03 15:03:06 +0100

    Add comments to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -91,5 +91,19 @@ CREATE TABLE store.cart (
 
 	PRIMARY KEY (id)
 );
+	CREATE TABLE store.comments (
+	id 				BIGINT 			UNSIGNED AUTO_INCREMENT,
+	product_id  	BIGINT      	UNSIGNED,
+	user_id     	BIGINT        	UNSIGNED NOT NULL,
+	rating 			TINYINT 		UNSIGNED,
+	comment_desc 	VARCHAR(512), 	
+	FOREIGN KEY (product_id) REFERENCES products(product_id),
+		--ON DELETE NO ACTION
+		--ON UPDATE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES users(user_id),
+		--ON DELETE DELETE
+		--ON UPDATE CASCADE,
+	primary key(id)
+);
 
 COMMIT;
